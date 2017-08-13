a0e09be28649621d4d96828fa3b5201af9592934
gazwald <gary.brandon@gmail.com>
2017-08-13 18:15:44 +1000

    Adding main code block and updating readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# quake2master
-Quake 2 Master server written in Python
+# Quake 2 Master
+
+This project originally started as a desire to aggregate the server lists for the various Quake 2 master servers.
+
+However most Quake 2 master servers these days appear to be dead and of the one that isn't (q2servers) it does not support b'query\n\0'.
+
+Goals for this project:
+* Write servers to DB backend - probably using PostgreSQL
+* Rest API for DB backend - probably using Flask
+* Front end for rest API - probably using VueJS
+* Unit tests for all!
+
+## Should I use this as a production Quake 2 master server?
+No. It's barely tested at the time of writing.
+
+## Testing
+Querying the server has only been tested using qstat and manually querying the server with Python.
+
+Quake 2 server code used: [yquake2](https://github.com/yquake2/yquake2) build 7.01 
+
+## Specs
+Specs were cobbled together using the [QStat](https://github.com/multiplay/qstat) source, [Quake 2](https://github.com/id-Software/Quake-2) source and tcpdump.
---
+++ master.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+import asyncio
+import ipaddress
+import struct
+
+LOCAL = ('127.0.0.1', 27900)
+serverlist = {}
+
+
+class MasterServer:
+    header = b'\xff\xff\xff\xff'
+    header_ack = b''.join([header, b'ack'])
+    header_servers = b''.join([header, b'servers '])
+
+    def connection_made(self, transport):
+        self.transport = transport
+
+    def decoder(self, code):
+        return code.decode('ascii')
+
+    def dictify(self):
+        """
+        Byte string -> Byte list -> Byte Tuple list -> Dict String
+        TODO: This should really be cleaned up...
+              and there's a mistake in one of the examples
+
+        Example input:
+        \\cheats\\0\\dmflags\\16\\\0
+        Example output:
+        [b'cheats', b'0', b'dmflags', b'16']
+        """
+        bytes_command = self.message[1].split(b'\\')[1:]
+        """
+        Example output:
+        [(b'cheats', b'0'), (b'dmflags', b'16')]
+        """
+        zipped = zip(bytes_command[0::2], bytes_command[1::2])
+        """
+        Example output:
+        {'cheats': '0', 'dmflags': '16'}
+        """
+        return dict((self.decoder(x), self.decoder(y)) for x, y in zipped)
+
+    def process_heartbeat(self, address):
+        serverlist[address] = self.dictify()
+
+    def process_shutdown(self, address):
+        """
+        Is this the required behaviour?
+        """
+        print(f"Shutdown from {address}")
+        del serverlist[address]
+
+    def process_ping(self, address):
+        print(f"Sending ack to {address}")
+        self.transport.sendto(self.header_ack, address)
+
+    def process_query(self, destination):
+        print(f"Sending servers to {destination}")
+        serverstring = [self.header_servers]
+        for server in serverlist.keys():
+            ip = int(ipaddress.IPv4Address(server[0]))
+            address = struct.pack('!LH', int(ip), server[1])
+            serverstring.append(address)
+
+        self.transport.sendto(b''.join(serverstring), destination)
+
+    def datagram_received(self, data, address):
+        self.message = data.split(b'\n')
+        if self.message[0].startswith(self.header):
+            command = self.message[0][4:]
+            if command.startswith(b"heartbeat"):
+                self.process_heartbeat(address)
+            elif command.startswith(b"shutdown"):
+                self.process_shutdown(address)
+            elif command.startswith(b"ping"):
+                self.process_ping(address)
+            else:
+                print(f"Unknown command: {command}")
+        elif self.message[0].startswith(b"query"):
+            self.process_query(address)
+        else:
+            print(f"Unknown command: {command}")
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    print("Starting UDP server")
+    # One protocol instance will be created to serve all client requests
+    listen = loop.create_datagram_endpoint(MasterServer, local_addr=LOCAL)
+    transport, protocol = loop.run_until_complete(listen)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    transport.close()
+    loop.close()
