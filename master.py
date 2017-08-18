a2154fcde6b4b993c614c21ecce0f3b04e2074a9
gazwald <gary.brandon@gmail.com>
2017-08-18 20:12:46 +1000

    State is now stored in SQLiteDB

--- master.py
+++ master.py
@@ -3,8 +3,11 @@ import asyncio
 import ipaddress
 import struct
 
-LOCAL = ('127.0.0.1', 27900)
-serverlist = {}
+from schema import Game, Version, Mapname, Gamename, Server
+from schema.functions import get_or_create
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
 class MasterServer:
@@ -17,6 +20,55 @@ class MasterServer:
 
     def decoder(self, code):
         return code.decode('ascii')
+
+    def update_server_in_db(self, address, data):
+        s = Session()
+        version = get_or_create(s, Version, name=data.get('version'))
+        mapname = get_or_create(s, Mapname, name=data.get('mapname'))
+        gamename = get_or_create(s, Gamename, name=data.get('gamename'))
+        server = s.query(Server).filter_by(
+            ip=address[0],
+            port=address[1]
+        ).first()
+        if server:
+            print("Updating DB")
+            server.hostname = data.get('hostname')
+            server.cheats = int(data.get('cheats'))
+            server.needpass = int(data.get('needpass'))
+            server.deathmatch = int(data.get('deathmatch'))
+            server.maxclients = int(data.get('maxclients'))
+            server.maxspectators = int(data.get('maxspectators'))
+            server.timelimit = int(data.get('timelimit'))
+            server.fraglimit = int(data.get('fraglimit'))
+            server.protocol = int(data.get('protocol'))
+            server.dmflags = int(data.get('dmflags'))
+            server.version = version
+            server.mapname = mapname
+            server.gamename = gamename
+            server.game = q2game
+        else:
+            print("Inserting DB")
+            server = Server(
+                hostname=data.get('hostname'),
+                ip=address[0],
+                port=address[1],
+                cheats=int(data.get('cheats')),
+                needpass=int(data.get('needpass')),
+                deathmatch=int(data.get('deathmatch')),
+                maxclients=int(data.get('maxclients')),
+                maxspectators=int(data.get('maxspectators')),
+                timelimit=int(data.get('timelimit')),
+                fraglimit=int(data.get('fraglimit')),
+                protocol=int(data.get('protocol')),
+                dmflags=int(data.get('dmflags')),
+                version=version,
+                mapname=mapname,
+                gamename=gamename,
+                game=q2game,
+            )
+            s.add(server)
+        s.commit()
+        s.close()
 
     def dictify(self):
         """
@@ -42,14 +94,17 @@ class MasterServer:
         return dict((self.decoder(x), self.decoder(y)) for x, y in zipped)
 
     def process_heartbeat(self, address):
-        serverlist[address] = self.dictify()
+        self.update_server_in_db(address, self.dictify())
 
     def process_shutdown(self, address):
         """
         Is this the required behaviour?
         """
         print(f"Shutdown from {address}")
-        del serverlist[address]
+        s = Session()
+        s.query(Server).filter_by(ip=address[0], port=address[1]).delete()
+        s.commit()
+        s.close()
 
     def process_ping(self, address):
         print(f"Sending ack to {address}")
@@ -58,7 +113,7 @@ class MasterServer:
     def process_query(self, destination):
         print(f"Sending servers to {destination}")
         serverstring = [self.header_servers]
-        for server in serverlist.keys():
+        for server in s.query(Server).with_entities(Server.ip, Server.port):
             ip = int(ipaddress.IPv4Address(server[0]))
             address = struct.pack('!LH', int(ip), server[1])
             serverstring.append(address)
@@ -84,9 +139,22 @@ class MasterServer:
 
 
 if __name__ == '__main__':
+    engine = create_engine('sqlite:///q2master.sqlite')
+    Session = sessionmaker(bind=engine)
+
+    """
+    Grab the game type(s) once at the start
+    """
+    s = Session()
+    q2game = get_or_create(s, Game, name='q2')
+    s.close()
+
+    """
+    asyncio magic
+    """
+    LOCAL = ('0.0.0.0', 27900)
     loop = asyncio.get_event_loop()
-    print("Starting UDP server")
-    # One protocol instance will be created to serve all client requests
+    print("Starting Quake 2 master server")
     listen = loop.create_datagram_endpoint(MasterServer, local_addr=LOCAL)
     transport, protocol = loop.run_until_complete(listen)
 
