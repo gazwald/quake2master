d509e1b4e8efea356b6d61c79a2fb1340ed30b44
gazwald <gary.brandon@gmail.com>
2018-02-23 23:13:46 +0000

    Warning: Untested code. Cleaning up previous commit.

--- app.py
+++ app.py
@@ -1,45 +1,31 @@
 #!/usr/bin/env python3
 import asyncio
-from datetime import datetime
 
 from database.functions import (create_db_session,
                                 create_db_conn)
 
-from masters import Quake2
+from masters import Common, Quake2
 
 
-class MasterServer:
-    def __init__(self):
-        self.header = b'\xff\xff\xff\xff'
-        self.header_ack = b''.join([self.header, b'ack'])
-        self.header_stat = b''.join([self.header, b'stat'])
-        self.header_servers = b''.join([self.header, b'servers '])
-
-    @staticmethod
-    def console_output(message):
-        print(f"{datetime.utcnow().isoformat()}: {message}")
-
+class MasterServer(Common):
     def connection_made(self, transport):
         self.transport = transport
 
     def process_stat(self, address):
-        self.transport.sendto(self.header_ack, address)
+        self.transport.sendto(b'stat_ack', address)
 
     def datagram_received(self, data, address):
         reply = None
         message = data.split(b'\n')
-        if message[0].startswith(self.header_stat):
+        if message[0].startswith(b'stat_ping'):
             reply = self.process_stat(address)
-        elif message[0].startswith(self.header):
+        elif message[0].startswith(self.q2header):
             command = message[0][4:]
             if command.startswith(b"heartbeat"):
-                self.console_output(f"Heartbeat from {address[0]}:{address[1]}")
-                q2.process_heartbeat(address)
+                reply = q2.process_heartbeat(address)
             elif command.startswith(b"shutdown"):
-                self.console_output(f"Shutdown from {address[0]}:{address[1]}")
-                q2.process_shutdown(address)
+                reply = q2.process_shutdown(address)
             elif command.startswith(b"ping"):
-                self.console_output(f"Sending ack to {address[0]}:{address[1]}")
                 reply = q2.process_ping(address)
             else:
                 self.console_output(f"Unknown command: {command}")
@@ -59,14 +45,13 @@ class MasterServer:
 
 
 if __name__ == '__main__':
-    """
-    asyncio magic
-    """
+    Common.console_output(f"Starting master server")
+
     engine = create_db_conn()
     session = create_db_session(engine)
     q2 = Quake2(session)
+
     loop = asyncio.get_event_loop()
-    MasterServer.console_output(f"Starting master server")
     listen = loop.create_datagram_endpoint(MasterServer,
                                            local_addr=('0.0.0.0', 27900))
     transport, protocol = loop.run_until_complete(listen)
@@ -76,7 +61,7 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         pass
 
-    MasterServer.console_output(f"Shutting down master server")
+    Common.console_output(f"Shutting down master server")
     session.close()
     transport.close()
     loop.close()
--- masters/__init__.py
+++ masters/__init__.py
@@ -1,18 +1,26 @@
 import ipaddress
 import struct
+from datetime import datetime
 
 from database.orm import Game, Server
 from database.functions import get_or_create
 
 
-class Master:
-    def __init__(self, session):
-        self.header = b'\xff\xff\xff\xff'
-        self.header_ack = b''.join([self.header, b'ack'])
-        self.header_servers = b''.join([self.header, b'servers '])
-        self.session = session
+class Common:
+    def __init__(self):
+        self.q2header = b'\xff\xff\xff\xff'
+        self.q2header_ack = b''.join([self.q2header, b'ack'])
+        self.q2header_servers = b''.join([self.q2header, b'servers '])
 
     @staticmethod
+    def console_output(message):
+        print(f"{datetime.utcnow().isoformat()}: {message}")
+
+
+class Master(Common):
+    def __init__(self, session):
+        self.session = session
+
     def bytepack(data):
         """
         Pack an IP (4 bytes) and Port (2 bytes) together
@@ -25,22 +33,13 @@ class Master:
 
 
 class Quake2(Master):
-    def fetch_servers(self):
-        serverstring = [self.header_servers]
-        for server in self.session.query(Server)\
-                                  .filter(Server.active)\
-                                  .with_entities(Server.ip,
-                                                 Server.port).all():
-            serverstring.append(self.bytepack(server))
-
-        return b''.join(serverstring)
-
     def get_server(self, address):
         return self.session.query(Server, address)\
                            .filter_by(ip=address[0],
                                       port=address[1]).first()
 
     def process_heartbeat(self, address):
+        self.console_output(f"Heartbeat from {address[0]}:{address[1]}")
         self.game = get_or_create(self.session, Game, name='q2')
         server = self.get_server(address)
         if not server:
@@ -52,16 +51,30 @@ class Quake2(Master):
             )
             self.session.add(server)
 
+        return None
+
     def process_shutdown(self, address):
+        self.console_output(f"Shutdown from {address[0]}:{address[1]}")
         server = self.get_server(address)
         if server:
             server.active = False
 
+        return None
+
     def process_ping(self, address):
+        self.console_output(f"Sending ack to {address[0]}:{address[1]}")
         server = self.get_server(address)
         if server:
             server.active = True
-        return self.header_ack
+
+        return self.q2header_ack
 
     def process_query(self):
-        return self.fetch_servers()
+        serverstring = [self.q2header_servers]
+        for server in self.session.query(Server)\
+                                  .filter(Server.active)\
+                                  .with_entities(Server.ip,
+                                                 Server.port).all():
+            serverstring.append(self.bytepack(server))
+
+        return b''.join(serverstring)
