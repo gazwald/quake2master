7aa06a7fb4e4af513819368ffc14c9c80a981c49
gazwald <gary.brandon@gmail.com>
2018-02-26 20:32:08 +1100

    Started work on basic tests

---
+++ tests/test_master.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+from masters import Master
+
+
+class TestMaster(TestCase):
+    def test_bytepack(self):
+        result = Master.bytepack(('127.0.0.1', 27910))
+        self.assertEqual(result, b'\x7f\x00\x00\x01m\x06')
+        result = Master.bytepack(('1.1.1.1', 1000))
+        self.assertEqual(result, b'\x01\x01\x01\x01\x03\xe8')
+        result = Master.bytepack(('254.254.254.254', 65535))
+        self.assertEqual(result, b'\xfe\xfe\xfe\xfe\xff\xff')
+
+    def test_is_q2(self):
+        self.assertTrue(Master.is_q2(b'\xff\xff\xff\xffping'))
+        self.assertTrue(Master.is_q2(b'\xff\xff\xff\xffheartbeat'))
+        self.assertTrue(Master.is_q2(b'\xff\xff\xff\xffshutdown'))
+        self.assertTrue(Master.is_q2(b'query'))
+        self.assertFalse(Master.is_q2(b'garbage'))
+        self.assertFalse(Master.is_q2(b'\xff\xff\xff\xff'))
+        self.assertFalse(Master.is_q2(b'\xff\xff\xff\xffgarbage'))
