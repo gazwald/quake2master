df91db32b2b844eaa7ec2427291b01f1e23ac669
gazwald <gary.brandon@gmail.com>
2018-02-27 02:13:09 +0000

    Adding unit tests for Quake2 class. Moved unit tests into unit_tests subdirectory in prep' for integration tests.

--- tests/unit_tests/test_master.py
+++ tests/unit_tests/test_master.py
@@ -4,15 +4,18 @@ from masters import Master
 
 
 class TestMaster(TestCase):
-    def test_bytepack(self):
+    def test_master_bytepack(self):
         result = Master.bytepack(('127.0.0.1', 27910))
+        self.assertEqual(len(result), 6)
         self.assertEqual(result, b'\x7f\x00\x00\x01m\x06')
         result = Master.bytepack(('1.1.1.1', 1000))
+        self.assertEqual(len(result), 6)
         self.assertEqual(result, b'\x01\x01\x01\x01\x03\xe8')
         result = Master.bytepack(('254.254.254.254', 65535))
+        self.assertEqual(len(result), 6)
         self.assertEqual(result, b'\xfe\xfe\xfe\xfe\xff\xff')
 
-    def test_is_q2(self):
+    def test_master_is_q2(self):
         self.assertTrue(Master.is_q2(b'\xff\xff\xff\xffping'))
         self.assertTrue(Master.is_q2(b'\xff\xff\xff\xffheartbeat'))
         self.assertTrue(Master.is_q2(b'\xff\xff\xff\xffshutdown'))
---
+++ tests/unit_tests/test_quake2.py
@@ -0,0 +1,44 @@
+from unittest import TestCase
+from unittest.mock import patch
+import logging
+
+from masters import Headers, Quake2
+
+
+class TestQuake2(TestCase):
+    def setUp(self):
+        self.q2 = Quake2(None)
+
+    def test_quake2(self):
+        self.assertIsInstance(self.q2, Quake2)
+
+    def test_quake2_process_request(self):
+        logging.disable(logging.CRITICAL)
+        result = self.q2.process_request(b'garbage', ('127.0.0.1', 29710))
+        self.assertEqual(result, None)
+
+    def test_quake2_process_request_log(self):
+        logging.disable(logging.NOTSET)
+        with self.assertLogs(level='INFO') as cm:
+            self.q2.process_request(b'garbage', ('127.0.0.1', 29710))
+        self.assertEqual(cm.output, ["WARNING:root:Unknown message: [b'garbage']"])
+
+    @patch('masters.Quake2.process_query')
+    def test_quake2_process_request_query(self, MockProcessQuery):
+        self.q2.process_request(Headers.q2query, ('127.0.0.1', 29710))
+        assert Quake2.process_query.called
+
+    @patch('masters.Quake2.process_ping')
+    def test_quake2_process_request_ping(self, MockProcessPing):
+        self.q2.process_request(Headers.q2header_ping, ('127.0.0.1', 29710))
+        assert Quake2.process_ping.called
+
+    @patch('masters.Quake2.process_shutdown')
+    def test_quake2_process_request_shutdown(self, MockProcessShutdown):
+        self.q2.process_request(Headers.q2header_shutdown, ('127.0.0.1', 29710))
+        assert Quake2.process_shutdown.called
+
+    @patch('masters.Quake2.process_heartbeat')
+    def test_quake2_process_request_heartbeat(self, MockProcessHeartbeat):
+        self.q2.process_request(Headers.q2header_heartbeat, ('127.0.0.1', 29710))
+        assert Quake2.process_heartbeat.called
