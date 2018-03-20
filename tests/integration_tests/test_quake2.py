02207768d8ab76d3d78e2928c73d6d7f964ba998
gazwald <gary.brandon@gmail.com>
2018-03-20 11:17:20 +0000

    Wrong database

--- tests/integration_tests/test_quake2.py
+++ tests/integration_tests/test_quake2.py
@@ -11,7 +11,7 @@ from masters import Headers, Quake2
 
 class TestQuake2(TestCase):
     def setUp(self):
-        self.engine = create_engine('postgresql://test_user:test_pass@127.0.0.1:5432/q2m')
+        self.engine = create_engine('postgresql://test_user:test_pass@localhost:5432/test_q2m')
         Base.metadata.create_all(self.engine)
         self.session = sessionmaker(bind=self.engine)
 
