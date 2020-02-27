56d5025878b0c2adf07c86c4bfdc684585cbc545
gazwald <gary.brandon@gmail.com>
2020-02-27 12:54:14 +1100

    ????

--- transport/__init__.py
+++ transport/__init__.py
@@ -10,38 +10,39 @@ from masterserver import MasterServer
 class Transport():
     def __init__(self, storage, protocols):
         logging.debug(f"{self.__class__.__name__ } - Initialising transport")
-        self.loop = asyncio.get_running_loop()
+        self.loop = asyncio.get_event_loop()
         self.signal()
         self.storage = storage
-        self.procotols = procotols
-        self.v4_udp_transport, self.v4_udp_protocol = self.listener(socket.AF_INET,
-                                                                    ('0.0.0.0', 27900))
-        """
-        self.v6_udp_transport, self.v6_udp_protocol = self.listener(socket.AF_INET6,
-                                                                    ('::', 27900))
-        """
+        self.protocols = protocols
+
+        self.listener(
+                socket.AF_INET,
+                ('0.0.0.0', 27900)
+        )
+
         self.v6_udp_transport = None
-        self.healt_check_transport = self.health_check()
+        self.health_check()
 
     def signal(self):
         logging.debug(f"{self.__class__.__name__ } - Setting up signals")
         signals = ('SIGINT', 'SIGTERM')
         for signame in signals:
-            self.loop.add_signal_handler(getattr(signal, signame),
-                                         functools.partial(self.shutdown, signame))
+            self.loop.add_signal_handler(
+                getattr(signal, signame),
+                functools.partial(self.shutdown, signame)
+            )
 
     async def listener(self,
                  socket_family,
                  bind):
+
         logging.debug(f"{self.__class__.__name__ } - Setting up master listener on {bind[0]}:{bind[1]}")
 
-        transport, protocol = await self.loop.create_datagram_endpoint(
-            MasterServer(self.storage, self.protocols),
+        self.v4_udp_transport, self.v4_udp_protocol = await self.loop.create_datagram_endpoint(
+            lambda: MasterServer(self.storage, self.protocols),
             family=socket_family,
             local_addr=bind
         )
-
-        return transport, protocol
 
     async def health_check(self,
                      socket_family=socket.AF_INET,
@@ -50,27 +51,25 @@ class Transport():
 
         logging.debug(f"{self.__class__.__name__ } - Setting up health check listener on {host}:{port}")
 
-        transport, protocol = await self.loop.create_server(
+        self.healt_check_transport, self.healt_check_protocol = await self.loop.create_server(
             lambda: HealthCheck(),
             family=socket_family,
             host=host,
             port=port)
 
-        return transport
-
-    def shutdown(self, sig):
+    async def shutdown(self, sig):
         logging.debug(f"{self.__class__.__name__ } - Caught {sig}")
         if self.healt_check_transport:
             logging.debug(f"{self.__class__.__name__ } - Closing health check listener")
-            self.healt_check_transport.close()
+            await self.healt_check_transport.close()
 
         if self.v4_udp_transport:
             logging.debug(f"{self.__class__.__name__ } - Closing IPv4 UDP transport")
-            self.v4_udp_transport.close()
+            await self.v4_udp_transport.close()
 
         if self.v6_udp_transport:
             logging.debug(f"{self.__class__.__name__ } - Closing IPv6 UDP transport")
-            self.v6_udp_transport.close()
+            await self.v6_udp_transport.close()
 
         self.loop.stop()
 
